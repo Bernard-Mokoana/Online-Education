a2268079a24ddc7ac13b735adbaa3b580fda91c0
Bernard <bernarddira@gmail.com>
2025-07-11 14:00:10 +0200

    Update transaction schema

--- backend/src/model/transaction.js
+++ backend/src/model/transaction.js
@@ -1,29 +1,32 @@
 import mongoose, { Schema } from "mongoose";
 
-const transactionSchema = new Schema(
-  {
-    studentId: {
-      type: Schema.Types.ObjectId,
-      ref: "user",
-    },
-    courseId: {
-      type: Schema.Types.ObjectId,
-      ref: "course",
-    },
-    amount: {
-      type: Number,
-      default: 0,
-    },
-    method: {
-      type: String,
-    },
-    status: {
-      type: String,
-    },
-    date: {
-      type: Date.now,
-      required: true,
-    },
+const transactionSchema = new Schema({
+  studentId: {
+    type: Schema.Types.ObjectId,
+    ref: "user",
+    required: true,
   },
-  { timestamps: true }
-);
+  courseId: {
+    type: Schema.Types.ObjectId,
+    ref: "course",
+    required: true,
+  },
+  amount: {
+    type: Number,
+  },
+  method: {
+    type: String,
+    enum: ["eft", "card", "cash", "wallet"],
+    default: "wallet",
+  },
+  status: {
+    type: String,
+    enum: ["pending", "success", "failed"],
+    default: "pending",
+  },
+  reference: String,
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});

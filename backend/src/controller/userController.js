5f5f7347e783591fa16830e1cb1087ef9014efc3
Bernard <bernarddira@gmail.com>
2025-07-12 22:24:09 +0200

    add user controller

---
+++ backend/src/controller/userController.js
@@ -0,0 +1,7 @@
+import { user } from "../model/user.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const register = async (req, res) => {};
+
+export const login = async (req, res) => {};

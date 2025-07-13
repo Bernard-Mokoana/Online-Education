32a7f5b541d54f70c2ccd4147a5611426ca9a19c
Bernard <bernarddira@gmail.com>
2025-07-13 17:40:43 +0200

    update the user api endpoints

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,9 @@ const app = express();
 app.use(express.json());
 
 import courseRoute from "./route/courseRoute.js";
+import userRoute from "./route/courseRoute.js";
 
 app.use("/api/v1/courses", courseRoute);
+app.use("/api/v1/user", userRoute);
 
 export default app;

ccd265d595cd5e82225715cf639a4575fa653feb
Bernard <bernarddira@gmail.com>
2025-07-13 17:41:20 +0200

    update the auth middleware

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -1,1 +1,35 @@
-const verifyJwt = async (req, res) => {};
+import { user } from "../model/user.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import { user } from "../model/user.js";
+
+dotenv.config({
+  path: "./.env",
+});
+
+const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
+
+export const verifyJwt = async (req, res, next) => {
+  const authHeader = req.header.authorization;
+
+  if (!authHeader || !authHeader.startWith("Bearer "))
+    return res.status(401).json({ message: "Unauthorized: No token" });
+
+  try {
+    const token = authHeader.split(" ")[1];
+    const decoded = jwt.verify(token, ACCESS_TOKEN_SECRET);
+
+    const User = user
+      .findById(decoded.userId)
+      .select("-password -refreshToken");
+
+    if (!User) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Token Invalid or expired" });
+  }
+};

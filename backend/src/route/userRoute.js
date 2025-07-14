59b96f9b06e89558b1a4656e9439d5aa6e6a363f
Bernard <bernarddira@gmail.com>
2025-07-14 19:30:56 +0200

    add user routes

--- backend/src/route/userRoute.js
+++ backend/src/route/userRoute.js
@@ -1,9 +1,21 @@
-import { register, login } from "../controller/userController.js";
+import {
+  register,
+  login,
+  getUserProfile,
+  updateUserProfile,
+  getAllUsers,
+} from "../controller/userController.js";
 import express from "express";
+import { verifyJwt, adminOnly } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router
+  .route("/profile")
+  .get(verifyJwt, getUserProfile)
+  .put(verifyJwt, updateUserProfile);
+router.get("/", verifyJwt, adminOnly, getAllUsers);
 
 export default router;

7a2d9b6cc80ae9bfcda11b3c8bf6669dc9479203
Bernard <bernarddira@gmail.com>
2025-07-15 08:18:52 +0200

    add stats api endpoint

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,8 +6,10 @@ app.use(express.json());
 
 import courseRoute from "./route/courseRoute.js";
 import userRoute from "./route/userRoute.js";
+import statsRoute from "./route/statsRoutes.js";
 
 app.use("/api/v1/courses", courseRoute);
 app.use("/api/v1/users", userRoute);
+app.use("/api/v1/stats", statsRoute);
 
 export default app;

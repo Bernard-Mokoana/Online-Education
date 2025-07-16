26ff2377353108fbb9f349c7b0a1aabe7b399104
Bernard <bernarddira@gmail.com>
2025-07-16 21:00:38 +0200

    add the lesson api endpoint

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,9 +7,11 @@ app.use(express.json());
 import courseRoute from "./route/courseRoute.js";
 import userRoute from "./route/userRoute.js";
 import statsRoute from "./route/statsRoutes.js";
+import lessonRoute from "./route/lessonRoute.js";
 
 app.use("/api/v1/courses", courseRoute);
 app.use("/api/v1/users", userRoute);
 app.use("/api/v1/stats", statsRoute);
+app.use("/api/v1/lessons", lessonRoute);
 
 export default app;

106e185bb97133d0ab269feb7ccd8c8d0567d59e
Bernard <bernarddira@gmail.com>
2025-07-16 19:57:50 +0200

    update the lessons schema

--- backend/src/model/lessons.js
+++ backend/src/model/lessons.js
@@ -1,22 +1,28 @@
 import mongoose, { Schema } from "mongoose";
 
-const lessonSchema = new Schema({
-  courseId: {
-    type: Schema.Types.ObjectId,
-    ref: "course",
-    required: true,
+const lessonSchema = new Schema(
+  {
+    courseId: {
+      type: Schema.Types.ObjectId,
+      ref: "course",
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    videoUrl: {
+      type: String,
+    },
+    content: {
+      type: String,
+    },
+    Order: {
+      type: Number,
+      default: 1,
+    },
   },
-  title: {
-    type: String,
-    required: true,
-  },
-  videoUrl: {
-    type: String,
-  },
-  content: {
-    type: String,
-  },
-  Order: Number,
-});
+  { timestamps: true }
+);
 
 export const lessons = mongoose.model("lessons", lessonSchema);

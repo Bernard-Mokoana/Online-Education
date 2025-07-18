9f5e41b6a45d139f8a5aa2999d9848f367d8c5f7
Bernard <bernarddira@gmail.com>
2025-07-18 19:40:05 +0200

    update the transaction api endpoint

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,6 +10,7 @@ import statsRoute from "./route/statsRoutes.js";
 import lessonRoute from "./route/lessonRoute.js";
 import enrollmentRoute from "./route/enrollmentRoute.js";
 import submissionRoute from "./route/submissionRoute.js";
+import transactionRoute from "./route/transactionRoute.js";
 
 app.use("/api/v1/courses", courseRoute);
 app.use("/api/v1/users", userRoute);
@@ -17,5 +18,6 @@ app.use("/api/v1/stats", statsRoute);
 app.use("/api/v1/lessons", lessonRoute);
 app.use("/api/v1/enrollments", enrollmentRoute);
 app.use("/api/v1/submission", submissionRoute);
+app.use("/api/v1/transaction", transactionRoute);
 
 export default app;

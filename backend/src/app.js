e39e402e75423088a0319776f84dbd16467fe5a7
Bernard <bernarddira@gmail.com>
2025-07-18 18:45:22 +0200

    Update the submission api endpoint

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,11 +9,13 @@ import userRoute from "./route/userRoute.js";
 import statsRoute from "./route/statsRoutes.js";
 import lessonRoute from "./route/lessonRoute.js";
 import enrollmentRoute from "./route/enrollmentRoute.js";
+import submissionRoute from "./route/submissionRoute.js";
 
 app.use("/api/v1/courses", courseRoute);
 app.use("/api/v1/users", userRoute);
 app.use("/api/v1/stats", statsRoute);
 app.use("/api/v1/lessons", lessonRoute);
 app.use("/api/v1/enrollments", enrollmentRoute);
+app.use("/api/v1/submission", submissionRoute);
 
 export default app;

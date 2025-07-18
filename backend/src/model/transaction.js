9b817a4a1e09bce9dad36fb28508ae71eabcfe71
Bernard <bernarddira@gmail.com>
2025-07-18 19:01:34 +0200

    update schema

--- backend/src/model/transaction.js
+++ backend/src/model/transaction.js
@@ -1,34 +1,35 @@
 import mongoose, { Schema } from "mongoose";
 
-const transactionSchema = new Schema({
-  studentId: {
-    type: Schema.Types.ObjectId,
-    ref: "user",
-    required: true,
+const transactionSchema = new Schema(
+  {
+    student: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+    course: {
+      type: Schema.Types.ObjectId,
+      ref: "course",
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    method: {
+      type: String,
+      enum: ["eft", "card", "cash", "wallet"],
+      default: "wallet",
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "success", "failed"],
+      default: "pending",
+    },
+    reference: String,
   },
-  courseId: {
-    type: Schema.Types.ObjectId,
-    ref: "course",
-    required: true,
-  },
-  amount: {
-    type: Number,
-  },
-  method: {
-    type: String,
-    enum: ["eft", "card", "cash", "wallet"],
-    default: "wallet",
-  },
-  status: {
-    type: String,
-    enum: ["pending", "success", "failed"],
-    default: "pending",
-  },
-  reference: String,
-  date: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  { timestamps: true }
+);
 
 export const Transaction = mongoose.model("Transaction", transactionSchema);

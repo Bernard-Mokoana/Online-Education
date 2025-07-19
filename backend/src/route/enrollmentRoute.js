a54d58398eadf0c063e3237a157e1750e3a29a7f
Bernard <bernarddira@gmail.com>
2025-07-19 15:33:41 +0200

    add enrollment route

--- backend/src/route/enrollmentRoute.js
+++ backend/src/route/enrollmentRoute.js
@@ -5,12 +5,23 @@ import {
   markLessonComplete,
   deleteEnrollment,
 } from "../controller/enrollmentController.js";
+import {
+  verifyJwt,
+  studentOnly,
+  adminOnly,
+} from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/enroll", enrollInCourse);
-router.get("/user/:userId", getUserEnrollments);
-router.patch("/complete", markLessonComplete);
-router.delete("/:id", deleteEnrollment);
+router.post("/enroll", verifyJwt, studentOnly, enrollInCourse);
+router.get(
+  "/user/:userId",
+  adminOnly,
+  studentOnly,
+  verifyJwt,
+  getUserEnrollments
+);
+router.patch("/complete", studentOnly, verifyJwt, markLessonComplete);
+router.delete("/:id", verifyJwt, studentOnly, adminOnly, deleteEnrollment);
 
 export default router;

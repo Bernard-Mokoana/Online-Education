16a54b300ff14119da9b5710cb5324cbdc0f08b4
Bernard <bernarddira@gmail.com>
2025-07-20 12:54:31 +0200

    update the submission route

--- backend/src/route/submissionRoute.js
+++ backend/src/route/submissionRoute.js
@@ -6,13 +6,14 @@ import {
   updateSubmissionGrade,
   deleteSubmission,
 } from "../controller/submissionController.js";
+import { verifyJwt, studentOnly } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/", createSubmission);
 router.get("/lesson/:lessonId", getSubmissionByLesson);
-router.get("/:id", getSubmissionById);
 router.patch("/:id/grade", updateSubmissionGrade);
-router.delete("/:id", deleteSubmission);
+
+router.route("/:id").get(getSubmissionById).delete(deleteSubmission);
 
 export default router;

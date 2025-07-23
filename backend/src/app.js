5252552140c920b67e1fe61dfb3ab6bbf9b0c26e
Bernard <bernarddira@gmail.com>
2025-07-23 11:08:43 +0200

    update the api endpoint

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,25 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: ".env",
+});
 
 const app = express();
 
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 import courseRoute from "./route/courseRoute.js";
 import userRoute from "./route/userRoute.js";
@@ -14,6 +31,7 @@ import transactionRoute from "./route/transactionRoute.js";
 import assessmentRoute from "./route/assessmentRoute.js";
 import reportRoute from "./route/reportRoute.js";
 import certificateRoute from "./route/certificateRoute.js";
+import progressRoute from "./route/progressRoute.js";
 
 app.use("/api/v1/courses", courseRoute);
 app.use("/api/v1/users", userRoute);
@@ -25,5 +43,6 @@ app.use("/api/v1/transaction", transactionRoute);
 app.use("/api/v1/assessments", assessmentRoute);
 app.use("/api/v1/reports", reportRoute);
 app.use("/api/v1/certificates", certificateRoute);
+app.use("/api/v1/progress", progressRoute);
 
 export default app;

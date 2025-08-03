49b4a6e162ddaf4c66d27f82c483d497d08b13f7
Bernard <bernarddira@gmail.com>
2025-08-03 18:33:20 +0200

    update the user schema

--- backend/src/model/user.js
+++ backend/src/model/user.js
@@ -32,6 +32,8 @@ const userSchema = new Schema(
       type: Boolean,
       default: false,
     },
+    resetPasswordToken: String,
+    resetPasswordEXpire: Date,
   },
   {
     timestamps: true,

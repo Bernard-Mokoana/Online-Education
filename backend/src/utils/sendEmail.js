86247315bf71b14937a37244bfa9ce9bbbecd3ca
Bernard <bernarddira@gmail.com>
2025-08-03 18:33:55 +0200

    update the send email util

---
+++ backend/src/utils/sendEmail.js
@@ -0,0 +1,28 @@
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: "./.env",
+});
+
+const sendEmail = async (to, subject, html) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+
+  await transporter.sendMail({
+    from: `"Course platform" <${process.env.EMAIL_USER}>`,
+    to,
+    subject,
+    html,
+  });
+};
+
+export default sendEmail;
